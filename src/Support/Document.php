5de90fa0439da3540fc7e8f6762027a5990667a3
Victor <victorhbbergamo@gmail.com>
2024-12-19 12:23:48 +0000

    implement cnpj method and choose method

--- src/Support/Document.php
+++ src/Support/Document.php
@@ -2,13 +2,53 @@
 
 namespace Upward\Formatters\Laravel\Support;
 
+use Exception;
+use Illuminate\Support\Str;
+use Illuminate\Support\Stringable;
 use Upward\Formatters\Document as DocumentFormatter;
+use Upward\Formatters\Documents\CnpjDocument;
 use Upward\Formatters\Documents\CpfDocument;
+use Upward\Formatters\Exceptions\Documents\InvalidDocumentException;
+use function Upward\Formatters\Helpers\Sanitization\only_digits;
 
 class Document
 {
     public function cpf(string $value): DocumentFormatter
     {
         return new DocumentFormatter(document: new CpfDocument($value));
+    }
+
+    public function cnpj(string $value): DocumentFormatter
+    {
+        return new DocumentFormatter(document: new CnpjDocument($value));
+    }
+
+    /**
+     * Choose a Document based on the given value.
+     */
+    public function choose(string $value): DocumentFormatter | null
+    {
+        try {
+            $value = Str::of(string: only_digits($value))
+                ->when(
+                    value: static fn (Stringable $string): bool => $string->length() < 11,
+                    callback: static fn (Stringable $string): Stringable => $string->padLeft(length: 11, pad: '0'),
+                )
+                ->when(
+                    value: static fn (Stringable $string): bool => $string->length() > 11,
+                    callback: static fn (Stringable $string): Stringable => $string->padLeft(length: 14, pad: '0'),
+                )
+                ->value();
+
+            return new DocumentFormatter(
+                document: match (Str::length($value)) {
+                    11 => new CpfDocument($value),
+                    14 => new CnpjDocument($value),
+                    default => throw InvalidDocumentException::make($value),
+                },
+            );
+        } catch (Exception) {
+            return null;
+        }
     }
 }
--- tests/src/Facades/DocumentFacadeTest.php
+++ tests/src/Facades/DocumentFacadeTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Upward\Formatters\Documents\CnpjDocument;
+use Upward\Formatters\Documents\CpfDocument;
 use Upward\Formatters\Laravel\Facades\Document;
 use Upward\Formatters\Laravel\Tests\Facades\TestCase;
 
@@ -9,3 +11,29 @@ it('should return Document instance', function () {
     expect(value: Document::cpf(value: '12345678901'))
         ->toBeInstanceOf(class: \Upward\Formatters\Document::class);
 });
+
+it('should be able choose CPF Document by the given value', function (string $value): void {
+    $document = Document::choose($value);
+
+    $reflector = new ReflectionClass($document);
+
+    expect(value: $reflector->getProperty(name: 'document')->getValue($document))
+        ->toBeInstanceOf(class: CpfDocument::class);
+})->with([
+    '12345678901',
+    '66988536078',
+    '12045683087',
+]);
+
+it('should be able choose CNPJ Document by the given value', function (string $value): void {
+    $document = Document::choose($value);
+
+    $reflector = new ReflectionClass($document);
+
+    expect(value: $reflector->getProperty(name: 'document')->getValue($document))
+        ->toBeInstanceOf(class: CnpjDocument::class);
+})->with([
+    '46730186000135b',
+    'd00562650000152',
+    '04593675000106',
+]);
